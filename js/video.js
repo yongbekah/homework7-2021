6c9d34cd81935b4388aaafdf85602957fa1f85e4
yongbekah <89883390+yongbekah@users.noreply.github.com>
2021-11-18 18:16:33 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,9 +1,9 @@
 var video = document.querySelector("#player1");
 
-// window.addEventListener("load", function() {
-// 	console.log("Good job opening the window")
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
 
-// });
+});
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
